2d0bea34e248529f1311949093785abf32dca003
SR-Delightfully <033.s.robinson@gmail.com>
2026-01-01 17:04:11 -0500

    adding layout for profile, subject to be changed in the future

--- app/Views/profileView.php
+++ app/Views/profileView.php
@@ -2,25 +2,59 @@
 $page_title = 'Home';
 ?>
 
-<div id="news-carousel">
+<div id="profile-title">
 </div>
 
-<div id="event-carousel">
+<div id="profile-visitors">
+    list of profile visitors
 </div>
 
-<div id="user-spotlight">
+<div id="profile-avatar">
+    profile avatar
 </div>
 
-<div id="pet-spotlight">
+<div id="profile-achievements">
+    list of profile's achievements
 </div>
 
-<div id="latest-posts">
+<div id="profile-contact">
+    widget with buttons interact with the profile
 </div>
 
-<div id="latest-threads">
+<div id="profile-about">
+    profile's about section
 </div>
 
-<div id="latest-activity">
+<div id="profile-signature">
+    profile's signature
 </div>
 
+<div id="profile-pet">
+    profile's main pet
+</div>
 
+<div id="profile-forum-stats">
+    <p>profile's form strip_tags</p>
+    <p># of threads made</p>
+    <p># of posts made</p>
+</div>
+
+<div id="profile-equipped-list">
+    list of items on the profile's avatar
+</div>
+
+<div id="profile-wish-list">
+    list of items on the profiles wishlist
+</div>
+
+<div id="profile-comments">
+    list of comments left from other profiles on this profile
+</div>
+
+<div id="profile-besties">
+    limited to 3 slots, profiles besties
+</div>
+
+<div id="profile-friends">
+    list of profiles friends, non besties
+</div>
